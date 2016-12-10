bc9897fcb2c3a4795d2fde6babef406f14ef83ff
ijzm <ignacio.zevallos@hotmail.com>
2016-12-09 21:48:07 -0500

    PlayerMovement

---
+++ Assets/Scripts/playerMovement.js
@@ -0,0 +1,15 @@
+﻿
+var rb: Rigidbody2D;
+var speed: int;
+
+function Start() {
+	rb = GetComponent. < Rigidbody2D > ();
+}
+
+function Update() {
+	rb.velocity = new Vector2(
+		Input.GetAxis("Horizontal") * speed,
+		Input.GetAxis("Vertical") * speed
+	);
+
+}
